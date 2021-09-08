1f82a404cc645bf2c06120104aa4de4ec58fd01e
Evan White <69662443+evanwwhite@users.noreply.github.com>
2021-09-08 14:56:23 -0500

    Update ArrayArrayListReveiw.java

--- ArrayArrayListReveiw.java
+++ ArrayArrayListReveiw.java
@@ -16,6 +16,7 @@ public class ArrayArrayListReveiw {
 		Scanner input = new Scanner(new File("input.txt"));
 		
 		ArrayList <Integer> list1 = new ArrayList <Integer>();
+		
 		Integer []list2 = new Integer[15];
 		
 		while(input.hasNext())
@@ -26,39 +27,34 @@ public class ArrayArrayListReveiw {
 
 			   counter++;
 			}
-		
-		
-		for(int i = 0; i < list2.length; i++)
-		{
-			
-			
-			
-			
-			
-		}
+		System.out.print("Start Array: ");
 		System.out.println(Arrays.toString(list2));
 		
-	
-		
+		for(int w = 0; w < list2.length; w++)
+		{
+			if(list2[w] == 19)
+			{
+				for(int r = w; r < list2.length - 1; r++)
+				{
+					list2[r] = list2[r+1];
+				}
+				list2[14] = -1;
+				System.out.println(Arrays.toString(list2));
+			}
+		}
+			
 		list2[3] = 9;
+		
+		for(int s = list2.length -1; s > 5; s--)
+		{
+			list2[s] = list2[s-1];
+		}
+		
 		list2[5] = 12;
+		System.out.print("End Result Array: ");
+		System.out.println(Arrays.toString(list2));
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		//=====
 		System.out.println("Start Array List" + list1);
 		
 		if(list1.contains(19))
@@ -84,7 +80,7 @@ public class ArrayArrayListReveiw {
 		list1.set(3, 9);
 		System.out.println("With 9 " + list1);
 		
-		list1.set(5, 12);
+		list1.add(5, 12);
 		System.out.println("With 12 " + list1);
 
 		
